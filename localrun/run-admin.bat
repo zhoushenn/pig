bbcfd0e31f6c8a4a69f14e474800deb3045d3ac1
zhoushen <445484792@qq.com>
2021-06-29 11:29:19 +0800

    添加doc一些文档.

--- localrun/run-admin.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-:: 修改目录为你的项目目录.
-start cmd /k "cd/d D:\zwlspace\pig-framework.git&&java -jar -Xms64M -Xmx64M -Dfile.encoding=utf-8  pig-upms/pig-upms-biz/target/pig-upms-biz.jar"
-start cmd /k "cd/d D:\zwlspace\pig-framework.git&&java -jar -Xms64M -Xmx64M -Dfile.encoding=utf-8  pig-visual/pig-codegen/target/pig-codegen.jar"
